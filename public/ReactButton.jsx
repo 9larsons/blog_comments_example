90394e6a3df2f6ae3e73395025e3321bf4178a00
Steve Larson <9larsons@gmail.com>
2022-05-22 15:19:13 -0500

    removed

--- public/ReactButton.jsx
+++
@@ -1,6 +0,0 @@
-const ReactButton = () => {
-  console.log(`react triggered`)
-  return <span><button>React</button></span>
-}
-
-ReactDOM.render(<ReactButton />, document.querySelector('#btn-react'));

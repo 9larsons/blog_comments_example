0bb05b9af866d4750b552cbf2eeed003d3f7616a
Steve Larson <9larsons@gmail.com>
2022-05-22 20:49:53 -0500

    restructure to controller, model, router

---
+++ models/comment.model.js
@@ -0,0 +1,49 @@
+const sql = require('./db.js');
+
+// constructor
+function Comment(comment) {
+  this.text = comment.text;
+  this.userId = comment.userId;
+  this.replyToId = comment.replyToId || null;
+}
+
+Comment.create = (newComment, result) => {
+  sql.query(
+    // alternatively could use the object values and [insert into comment SET ?] but we would
+      // lose the server setting the time in that way
+    `insert into comments (text,userId,instant,replyToId) values (?,?,now(),?);`,
+    Object.values(newComment), // sql wants an array, but we want the constructor to be an object
+    (error, res) => {
+      if (error) {
+        console.log(`error creating comment`, error);
+        result(error, null)
+        return;
+      }
+      result(null, res)
+    }
+  )
+}
+
+Comment.getAll = result => {
+  sql.query(
+    `select comments.*,coalesce(upvotes.count,0) upvotes
+    from comments
+      left join (
+        select commentId,count(*) count
+            from upvotes
+            group by commentId
+      ) upvotes
+        on upvotes.commentId = comments.id
+    order by comments.instant;`,
+    (error, res) => {
+      if (error) {
+        console.log(`error creating comment`, error);
+        result(error, null)
+        return;
+      }
+      result(null, res)
+    }
+  )
+}
+
+module.exports = Comment;
--- server.js
+++ server.js
@@ -12,99 +12,13 @@ app.use(express.static('public')) // make everything in /public accessible
 app.use(express.json()) // don't need to install body-parser any more for JSON parsing
 
 // prepare mysql database
-const mysql = require('mysql')
-const res = require('express/lib/response')
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'comments_schema'
-})
+const connection = require('./models/db')
 
 // ... connect
 connection.connect(error => {
   if (error) throw error
   console.log("Connected to the database.")
 }) 
-
-// add a new comment
-app.post("/api/addComment", (request, response) => {
-  const newCommentValues = [
-    request.body.comment, // comment text
-    request.body.userId, // user ID
-    request.body.replyToId // only populated if a reply
-  ]
-  connection.query(
-    // let database create timestamp for now...
-    `insert into comments (text,userId,instant,replyToId) values (?,?,now(),?);`,
-    newCommentValues,
-    function (error, data) {
-      if (error) console.log(error)
-      response.status(201).json({
-        status: "success",
-        message: "comment created",
-      })
-    }
-  )
-})
-
-// get all comments (sorted by date posted, oldest > newest)
-app.get('/api/getComments', (request, response) => {
-  connection.query(
-    `select comments.*,coalesce(upvotes.count,0) upvotes
-    from comments
-      left join (
-        select commentId,count(*) count
-            from upvotes
-            group by commentId
-      ) upvotes
-        on upvotes.commentId = comments.id
-    order by comments.instant;`,
-    function (error, data) {
-      if (error) console.log(error)
-      response.send(data)
-    }
-  )
-})
-
-// add upvote
-app.post('/api/upvote', (request, response) => {
-  const upvote = [
-    request.body.id, // commentId
-    request.body.userId // user Id
-  ]
-  connection.query(
-    // let database create timestamp for now...
-    `insert into upvotes (commentId,userId,instant) values (?,?,now());`,
-    upvote,
-    function (error, data) {
-      if (error) console.log(error)
-      response.status(201).json({
-        status: "success",
-        message: "upvote added",
-      })
-    }
-  )
-})
-
-app.delete('/api/upvotes', (request, response) => {
-  const upvote = [
-    request.body.id, // commentId
-    request.body.userId, // user ID
-  ]
-  connection.query(
-    // let database create timestamp for now...
-    `delete from upvotes where commentId = ? and userId = ?;`,
-    upvote,
-    function (error, data) {
-      if (error) console.log(error)
-      response.status(201).json({
-        status: "success",
-        message: "upvote deleted",
-      })
-    }
-  )
-})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'))
@@ -122,6 +36,28 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('upvote added', id)
   })
 })
+
+
+const comments = require('./controllers/comments.controller');
+const upvotes = require('./controllers/upvotes.controller');
+
+const router = require("express").Router();
+
+// new comment
+router.post('/api/comments', comments.create);
+
+// get all comments
+router.get('/api/comments', comments.getAll);
+
+// add upvote
+router.post('/api/upvotes', upvotes.create);
+
+// delete upvote
+router.delete('/api/upvotes', upvotes.delete);
+
+
+app.use('', router)
+
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

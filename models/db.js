4e21f711f283bcd4b2f2fc2de37d1558c23124e4
Steve Larson <9larsons@gmail.com>
2022-05-26 09:56:36 -0500

    move to connection pool

--- models/db.js
+++ models/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 const dbConfig = require('../config/db.config.js');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB,
+    connectionLimit: 100,
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -11,15 +11,6 @@ const port = 8080
 app.use(express.static('public')) // make everything in /public accessible
 app.use(express.json()) // don't need to install body-parser any more for JSON parsing
 
-// prepare mysql database
-const connection = require('./models/db')
-
-// ... connect
-connection.connect(error => {
-  console.log(`Connected to the database.`)
-  if (error) throw error
-})
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
